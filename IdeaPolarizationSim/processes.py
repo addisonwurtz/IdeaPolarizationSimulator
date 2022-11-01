59a7c99d732d14179010227e4a2396ca6c26f173
addisonwurtz <awurtz@pdx.edu>
2022-10-31 19:47:18 -0700

    basic outline of data structures and core classes complete. Need to make some decisions about how much to lean into OOP for this project.

--- IdeaPolarizationSim/processes.py
+++ IdeaPolarizationSim/processes.py
@@ -1,9 +1,28 @@
-user_connections = {}
-user_opinions = {}
-user_connection_strength = {}
+import simpy
+
+user_connection_graph = {int: list[int]}  # adjacency list representation of graph
+user_opinions = {int: float}  # maps user to opinion value between -1 and 1
+user_connection_strengths = {tuple, float}  # maps edges to connection strength value between 0 and 1
+news_item_opinion_scores = {int, float}  # maps news item to opinion value between -1 nd 1
+initial_spreader_nodes = {NewsItem: list[int]}
+update_rate = 0.1  # update rate for opinion and connection strength
 
 
-class Person(object):
-    def __init__(self, env):
+class Person:
+    def __init__(self, env: simpy.Environment, node_name: int, opinion: float):
         self.env = env
+        self.node = node_name
+        self.opinion = opinion
+
+
+class Bot:
+    def __init__(self, env: simpy.Environment, node_name: int):
+        self.env = env
+        self.node = node_name
+
+
+class NewsItem:
+    def __init__(self, env: simpy.Environment, opinion: float):
+        self.env = env
+        self.opinion = opinion
 
