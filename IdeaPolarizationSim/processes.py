00c59f0274611b6109a4f21e1f2b7ed8baea4071
addisonwurtz <awurtz@pdx.edu>
2022-11-01 20:41:15 -0700

    Got rid of all standalone variables, and moved all info into objects.

--- IdeaPolarizationSim/processes.py
+++ IdeaPolarizationSim/processes.py
@@ -1,22 +1,36 @@
 import simpy
 
 
-class Person:
-    def __init__(self, env: simpy.Environment, node_name: int, opinion: float):
+class Network:
+    def __init__(self, env: simpy.Environment, nodes: [], edge_weights: {}):
         self.env = env
-        self.node = node_name
+        self.nodes = nodes
+        self.edge_weights = edge_weights
+
+
+class Person:
+    def __init__(self, env: simpy.Environment, name: int, opinion: float, connections: list, update_rate=0.1):
+        self.env = env
+        self.node = name
         self.opinion = opinion
+        self.connections = connections
+        self.update_rate = update_rate
+
+    def add_connections(self, connections):
+        self.connections.append(connections)
+        # TODO should probably update edge_weights anytime connection is added?
+
+    def share_news_item(self):
+        pass
+
+    def update_connection_strength(self):
+        pass
 
 
 class NewsItem:
-    def __init__(self, env: simpy.Environment, opinion: float):
+    def __init__(self, env: simpy.Environment, opinion_score: float, initial_spreader_nodes: []):
         self.env = env
-        self.opinion = opinion
+        self.opinion_score = opinion_score
+        self.initial_spreader_nodes = initial_spreader_nodes
 
 
-user_connection_graph = {Person: list[Person]}  # adjacency list representation of graph
-user_opinions = {Person: float}  # maps user to opinion value between -1 and 1
-user_connection_strengths = {tuple(Person, Person), float}  # maps edges to connection strength value between 0 and 1
-news_item_opinion_scores = {NewsItem, float}  # maps news item to opinion value between -1 and 1
-initial_spreader_nodes = {NewsItem: list[Person]}  # each news item has a list of initial spreaders
-update_rate = 0.1  # update rate for opinion and connection strength
