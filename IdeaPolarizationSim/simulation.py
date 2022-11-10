69ca5e487d8642b56cda0e76a735c2382841e54a
addisonwurtz <awurtz@pdx.edu>
2022-11-09 16:04:06 -0800

    Added unit tests for all simulation classes
    plus some small clean-up changes to simulation_classes.py

--- IdeaPolarizationSim/simulation.py
+++ IdeaPolarizationSim/simulation.py
@@ -9,9 +9,9 @@ import toy_graph
 
 update_rate = 0.1
 users: [User] = toy_graph.nodes
-news_items: [NewsItem] = [NewsItem(0, [toy_graph.user_1, toy_graph.user_6])]
+news_items: [NewsItem] = [NewsItem(1, 0, [toy_graph.user_1, toy_graph.user_6])]
 
-social_network = SocialNetwork(toy_graph.graph, users, news_items, update_rate)
+social_network = SocialNetwork(toy_graph.graph, news_items, update_rate)
 
 time = 1
 
@@ -21,14 +21,17 @@ for news_item in social_network.news_items:
     print(f'Nodes that have been infected by news item: {[user.user_id for user in news_item.inoculated_users]}')
 
     for user in news_item.infectious_users:
-        social_network.share_news_item(user, news_item)
+        try:
+            social_network.share_news_item(user, news_item)
+        except Exception as e:
+            print(e)
 
     time += 1
 
 for news_item in social_network.news_items:
     print(f'Number of users infected with story: {len(news_item.inoculated_users)}')
     print(f'Users infected with story: {[inoculated_user.user_id for inoculated_user in news_item.inoculated_users]}')
-
+    print(f'Simulation time: {time}')
 
 
 
